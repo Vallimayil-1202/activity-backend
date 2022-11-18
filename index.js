d22ae00e88823fc3166f1205d22d6aa20772db2c
Vallimayil-1202 <97376309+Vallimayil-1202@users.noreply.github.com>
2022-11-18 12:43:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require("express")
 const mongoose = require("mongoose")
 const routes = require("./routes")
 
-
-
 mongoose
 	.connect("mongodb+srv://user:pass@activity.7cfdpcb.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
 	.then(() => {
 		const app = express()
 		app.use(express.json())
 		app.use("/api", routes)
-		app.listen(5000, () => {
+		var port_number = server.listen(process.env.PORT || 3000);
+		app.listen(port_number, () => {
 			console.log("Server has started!")
 		})
 	})
